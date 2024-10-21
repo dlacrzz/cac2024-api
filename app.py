eb74851ec1fbdd630c6a6b7f5c5db7a5e98efdeb
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2024-10-20 21:00:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import requests
 import os
 import tempfile
 
 app = Flask(__name__)
 
 # INSERT MODEL BELOW
-model = load_model('saved_model.h5')
+model = load_model('models/saved_model.h5')
 
 def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(32, 32))
@@ -18,31 +17,18 @@ def preprocess_image(img_path):
     img /= 255.
     return img
 
-def download_image(url):
-    """Downloads the image from the provided URL and saves it temporarily."""
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        raise Exception('Failed to download image')
-    
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
-        tmp.write(response.content)
-        return tmp.name  # Return the temp file path
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    
-    if 'url' not in data:
-        return jsonify({'error': 'No URL provided'})
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
 
-    image_url = data['url']
+    file = request.files['file']
 
-    try:
-        img_path = download_image(image_url)
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    
+    # Use tempfile to handle the temporary directory
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        file.save(tmp.name)
+        img_path = tmp.name
+
     img = preprocess_image(img_path)
     prediction = model.predict(img)
 
@@ -51,9 +37,6 @@ def predict():
         'class_index': int(np.argmax(prediction)),  # Convert numpy int64 to Python int
         'confidence': float(np.max(prediction))  # Convert numpy float64 to Python float
     }
-
-    # Clean up the temporary file
-    os.remove(img_path)
 
     return jsonify(response)
 
